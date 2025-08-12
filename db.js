b95ccfffbebf3e0d810a558a9f08326ad1316cce
Praveen Gouda <110727148+Praveenggouda@users.noreply.github.com>
2025-08-12 19:29:08 +0530

    Add files via upload

---
+++ db.js
@@ -0,0 +1,16 @@
+// db.js
+const mysql = require('mysql2/promise');
+require('dotenv').config(); // Load variables from .env
+
+const db = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  timezone: 'local',
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
+});
+
+module.exports = db;
