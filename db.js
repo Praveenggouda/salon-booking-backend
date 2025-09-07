66b13ceacada83bdac6bbcb7cbe2a19e9e9512fb
Praveen Gouda <110727148+Praveenggouda@users.noreply.github.com>
2025-09-08 04:21:23 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,24 +1,17 @@
-import mysql from "mysql2";
+import mysql from 'mysql2/promise';
+import dotenv from 'dotenv';
+dotenv.config();
 
-const pool = mysql.createPool({
-  host: process.env.MYSQL_ADDON_HOST,
-  user: process.env.MYSQL_ADDON_USER,
-  password: process.env.MYSQL_ADDON_PASSWORD,
-  database: process.env.MYSQL_ADDON_DB,
-  port: process.env.MYSQL_ADDON_PORT,
-  ssl: {
-    rejectUnauthorized: false // important for Clever Cloud
-  }
+const db = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
 });
 
-// Test connection
-pool.getConnection((err, connection) => {
-  if (err) {
-    console.error("❌ MySQL Connection Error:", err);
-  } else {
-    console.log("✅ Connected to Clever Cloud MySQL");
-    connection.release();
-  }
-});
+export default db;
 
-export default pool;
+
