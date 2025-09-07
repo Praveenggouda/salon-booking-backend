f320ae1818f7e003d3d12ad964271ff4b7839782
Praveen Gouda <110727148+Praveenggouda@users.noreply.github.com>
2025-09-08 04:30:37 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,13 +1,11 @@
 import mysql from 'mysql2/promise';
-import dotenv from 'dotenv';
-dotenv.config();
 
 const db = mysql.createPool({
-  host: process.env.MYSQL_ADDON_HOST,
-  user: process.env.MYSQL_ADDON_USER,
-  password: process.env.MYSQL_ADDON_PASSWORD,
-  database: process.env.MYSQL_ADDON_DB,
-  port: process.env.MYSQL_ADDON_PORT,
+  host: 'bcnineplj7dyjhrseuiu-mysql.services.clever-cloud.com',
+  user: 'uq6vpcfofhquauek',          // fill from MYSQL_ADDON_USER
+  password: 'okCTiQ7eN5BjKnz7cX96',  // fill from MYSQL_ADDON_PASSWORD
+  database: 'bcnineplj7dyjhrseuiu', // from MYSQL_ADDON_DB
+  port: 3306,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
