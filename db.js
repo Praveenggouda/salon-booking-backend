be4e0ac3b41c14f7d447f2f46eb398421512dd7e
Praveen Gouda <110727148+Praveenggouda@users.noreply.github.com>
2025-09-08 22:42:24 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,16 +1,17 @@
 import mysql from 'mysql2/promise';
 
 const db = mysql.createPool({
-  host: 'bcnineplj7dyjhrseuiu-mysql.services.clever-cloud.com',
-  user: 'uq6vpcfofhquauek',          // fill from MYSQL_ADDON_USER
-  password: 'okCTiQ7eN5BjKnz7cX96',  // fill from MYSQL_ADDON_PASSWORD
-  database: 'bcnineplj7dyjhrseuiu', // from MYSQL_ADDON_DB
-  port: 3306,
+MYSQL_ADDON_HOST: 'bcnineplj7dyjhrseuiu-mysql.services.clever-cloud.com',
+MYSQL_ADDON_USER: 'uq6vpcfofhquauek',          // fill from MYSQL_ADDON_USER
+MYSQL_ADDON_PASSWORD: 'okCTiQ7eN5BjKnz7cX96',  // fill from MYSQL_ADDON_PASSWORD
+MYSQL_ADDON_DB: 'bcnineplj7dyjhrseuiu', // from MYSQL_ADDON_DB
+MYSQL_ADDON_PORT: 3306,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
 });
 
 export default db;
+
 
 
